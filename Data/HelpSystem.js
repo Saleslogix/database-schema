d079b0f8119a3f029d06a823b0d51b7b26225d1d
Angie Fredrick <angie.fredrick@swiftpage.com>
2014-02-12 13:25:29 -0700

    updated help files - table styles and broken links

--- Data/HelpSystem.js
+++ Data/HelpSystem.js
@@ -1,5 +1,5 @@
 CMCXmlParser._FilePathToXmlStringMap.Add(
 	'HelpSystem',
 	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
-	'<WebHelpSystem DefaultUrl=\"Content/SalesLogix_Database_Help_System.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"Content/Glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/SkinWebHelp2/Skin.xml\" Skins=\"NewWindow,SalesClient,WebHelp2\" BuildTime=\"2/7/2014 3:39:49 PM\" BuildVersion=\"9.1.2.0\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"false\" />'
+	'<WebHelpSystem DefaultUrl=\"Content/SalesLogix_Database_Help_System.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"Content/Glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/SkinWebHelp2/Skin.xml\" Skins=\"NewWindow,SalesClient,WebHelp2\" BuildTime=\"2/12/2014 1:18:32 PM\" BuildVersion=\"9.1.2.0\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"false\" />'
 );
